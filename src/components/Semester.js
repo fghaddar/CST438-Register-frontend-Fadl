cca3b2e6e5b9cbe5a8b213bca184e3a4b5d8b18b
fghaddar <80842831+fghaddar@users.noreply.github.com>
2021-10-05 22:56:38 -0700

    Update Semester.js

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -99,6 +99,7 @@ class Semester extends Component {
               </Button>
               <Button> 
                 <AddStudent addStudent = {this.addStudent} />
+                studentButton
               </Button>
           </div>
               <ToastContainer autoClose={9000} />   
